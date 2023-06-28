a802e7700363f6386dd1884b84a365019ececaa6
Artem <108129792+Langepas94@users.noreply.github.com>
2023-06-28 11:10:33 +0300

    Initial Commit

---
+++ Food_app_SwiftUI/Food_app_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_app_SwiftUIApp.swift
+//  Food_app_SwiftUI
+//
+//  Created by Artem on 28.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Food_app_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
